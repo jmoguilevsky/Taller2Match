6ec6fd2e610dc3e18fb1c6baa82de9623f36ca55
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-03-29 23:51:19 -0300

    Demo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
+var pg = require('pg');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -18,8 +19,19 @@ app.get('/cool', function(request, response) {
   response.send(cool());
 });
 
+app.get('/db', function (request, response) {
+    pg.defaults.ssl = true;
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
