2d73b98c3b262a485178fd1301ce40c1e41be4fb
SebaElizalde <sebi.elizalde@gmail.com>
2016-04-24 11:35:07 -0300

    Android App con: Login Register Welcome screen (esqueleto) SessionManager SQLiteHandler Config (ahi van las URLs del server)

---
+++ AndroidApp/MatcherApp/app/src/main/java/com/taller2/matcherapp/app/AppConfig.java
@@ -0,0 +1,12 @@
+package com.taller2.matcherapp.app;
+
+/**
+ * Created by seba on 24/04/16.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "aca_va_url_del_server";
+
+    // Server user register url
+    public static String URL_REGISTER = "aca_va_url_del_server";
+}
