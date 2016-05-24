186e74c1a9b920d6fe301ad63046fe4c718e8161
SebaElizalde <sebi.elizalde@gmail.com>
2016-05-24 18:50:58 -0300

    igual al commit hecho al master hecho hace 2 dias

--- AndroidApp/MatcherApp/app/src/main/java/com/taller2/matcherapp/app/AppConfig.java
+++ AndroidApp/MatcherApp/app/src/main/java/com/taller2/matcherapp/app/AppConfig.java
@@ -4,9 +4,15 @@ package com.taller2.matcherapp.app;
  * Created by seba on 24/04/16.
  */
 public class AppConfig {
-    // Server user login url
-    public static String URL_LOGIN = "http://10.0.2.2:7000/login";
+    // Server user login url: POST
+    public static String URL_LOGIN = "http://10.0.2.2:7000/users/login";
 
-    // Server user register url
-    public static String URL_REGISTER = "http://10.0.2.2:7000/signup";
+    // Server user register url: POST
+    public static String URL_REGISTER = "http://10.0.2.2:7000/users/signup";
+
+    // Server update user profile url: PUT
+    public static String URL_UPDATE = "http://10.0.2.2:7000/users/update";
+
+    // Server find candidate url: GET
+    public static String URL_FIND_CANDIDATE = "http://10.0.2.2:7000/users/match/update";
 }
